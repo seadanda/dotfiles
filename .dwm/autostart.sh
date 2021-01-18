058a8bfc95c2d29b1aeeaf4da5c0d32703950121
Dónal Murray <donal.murray@cern.ch>
2021-01-18 12:20:09 +0000

    Start dwm in a loop, do not start protonmail bridge until I open thunderbird

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 ~/.fehbg &
-pulseaudio --start &
 dunst &
-pcmanfm -d &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 nm-applet &
 blueman-applet &
-redshift -l 54.59:5.93 &
-/usr/lib/protonmail/bridge/protonmail-bridge --no-window &
+( ps -e | grep "[r]edshift" || redshift ) &
+wmname compiz &
